941000d95a9e17f6c0500311408bc8147bbe4d2c
vmio vmio <dohien11091999@gmail.com>
2019-08-07 17:43:15 +0700

    Initial Commit

---
+++ DemoAutoPushGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAutoPushGPS
+//
+//  Created by vmio vmio on 8/7/19.
+//  Copyright © 2019 AutoPushGPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
