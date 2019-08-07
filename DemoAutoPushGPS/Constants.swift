4354095ac5bb78cc4449468a186de0cbe2f43e12
vmio vmio <dohien11091999@gmail.com>
2019-08-07 18:44:56 +0700

    first commit

---
+++ DemoAutoPushGPS/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  DemoAutoPushGPS
+//
+//  Created by vmio vmio on 8/7/19.
+//  Copyright © 2019 AutoPushGPS. All rights reserved.
+//
+
+import Foundation
---
+++ DemoAutoPushGPS/Logger.swift
@@ -0,0 +1,9 @@
+//
+//  Logger.swift
+//  DemoAutoPushGPS
+//
+//  Created by vmio vmio on 8/7/19.
+//  Copyright © 2019 AutoPushGPS. All rights reserved.
+//
+
+import Foundation
